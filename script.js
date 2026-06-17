a1c586e2d6d11d8ee13d74d7d670f06704065d88
Sks <124144700+sks123git@users.noreply.github.com>
2026-06-18 01:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+let label = document.createElement("p");
+label.setAttribute("id", "counter");
+label.style.color = "#000000";
+label.style.fontSize = "12px";
+label.textContent = "0";
+label.style.display = "Block"
+
+let btn = document.createElement("button");
+btn.setAttribute("id", "incrementBtn");
+btn.textContent = "Increment";
+
+document.body.append(label);
+document.body.append(btn);
+
+let count = 0
+function increment() {
+    alert(count)
+    count++
+    label.textContent = count
+}
+
+btn.addEventListener("click", increment)
